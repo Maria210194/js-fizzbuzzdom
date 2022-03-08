c69c27f4db57f3df170b2146f8a5fe1619ac785a
Maria Stachie <mary.stachie@gmail.com>
2022-03-08 21:01:03 +0100

    layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let m;
 const container = document.querySelector("#container");
 
+/* VERSIONE ELEMENT
 
 for (let m = 1; m <= 100; m++) {
 
@@ -24,3 +25,31 @@ for (let m = 1; m <= 100; m++) {
         container.innerHTML += element;
     }
 }
+
+*/
+
+// VERSIONE CON ELEMENT PERSONALIZZATO (costante element non ripetuta)
+
+for (let m = 1; m <= 100; m++) {
+
+    if (m % 3 === 0 && m % 5 === 0) {
+        const fizzBuzz = `<div class="square FizzBuzz">FizzBuzz</div>`;
+        container.innerHTML += fizzBuzz;
+    }
+
+    else if (m % 3 === 0) {
+        const fizz = `<div class="square Fizz">Fizz</div>`;
+        container.innerHTML += fizz;
+    }
+
+    else if (m % 5 === 0) {
+        const buzz = `<div class="square Buzz">Buzz</div>`;
+        container.innerHTML += buzz;
+    }
+
+    else {
+        const number = `<div class="square">${m}</div>`;
+        container.innerHTML += number;
+    }
+}
+
